db29b822e5f5121094e0a5ddc392ac5ede94164a
Aitor González Jiménez <agonzalezj99@educarex.es>
2026-04-21 10:49:26 +0200

    cambios

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,13 +2,26 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li><NavLink to="/">Inicio</NavLink></li>
-        <li><NavLink to="/horarios">Horarios</NavLink></li>
-        <li><NavLink to="/registro">Registro</NavLink></li>
-        <li><NavLink to="/contacto">Contacto</NavLink></li>
-      </ul>
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid justify-content-center">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#menu"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse justify-content-center" id="menu">
+          <ul className="navbar-nav">
+            <li className="nav-item"><NavLink className="nav-link" to="/">Inicio</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/horarios">Horarios</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/registro">Registro</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link" to="/contacto">Contacto</NavLink></li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
