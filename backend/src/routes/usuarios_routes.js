96dbf750bdff33767e7ef019b6e360795e43f37a
Aitor González Jiménez <agonzalezj99@educarex.es>
2026-04-27 12:25:20 +0200

    fin

--- backend/src/routes/usuarios_routes.js
+++ backend/src/routes/usuarios_routes.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, getPerfil, updatePerfil, deletePerfil } = require("../controllers/usuarios_controller");
+const { register, login } = require("../controllers/usuarios_controller");
 const { auth } = require("../middleware/auth_middleware");
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/perfil", auth, getPerfil);
-router.put("/perfil", auth, updatePerfil);
-router.delete("/perfil", auth, deletePerfil);
 
 module.exports = router;
