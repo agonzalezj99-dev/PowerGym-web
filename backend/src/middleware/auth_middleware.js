a20c7c6d307d5e9c9df6f0beede6c38b7d0c0985
Aitor González Jiménez <agonzalezj99@educarex.es>
2026-05-11 22:16:35 +0200

    Ignorar archivos .env

--- backend/src/middleware/auth_middleware.js
+++ backend/src/middleware/auth_middleware.js
@@ -1,7 +1,6 @@
 const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
-  console.log("AUTH HEADER:", req.headers.authorization);
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
